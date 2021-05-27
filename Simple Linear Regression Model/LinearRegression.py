0697ceb45f81282d24a2be0239e415a596c2825f
gokulkrishnar1999 <84931647+gokulkrishnar1999@users.noreply.github.com>
2021-05-28 00:03:20 +0530

    Update LinearRegression.py

--- Simple Linear Regression Model/LinearRegression.py
+++ Simple Linear Regression Model/LinearRegression.py
@@ -5,6 +5,6 @@ y = dataset["marks"]
 x = dataset["hrs"].values.reshape(5,1)
 mind = LinearRegression()
 mind.fit(x,y)
-hours = int(input("Please enter the number of hours studied"))
+hours = int(input("Please enter the number of hours studied: "))
 result = mind.predict([[hours]])
 print("Your predicted marks would be: {0}".format(result))
